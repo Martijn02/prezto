ce1eb543f8d0b6dcdd37130832cf8de5e3756886
Martijn van Zal <martijn@vanzal.com>
2019-10-22 11:15:43 +0200

    Change history-substring-search to match only the beginning of commands

--- install.zsh
+++ install.zsh
@@ -4,3 +4,6 @@ setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
+
+# Change history-substring-search to match only the beginning of commands
+sed --in-place=.bak s/\\\*\\$\{escaped_query\}/\$\{escaped_query\}/ ${ZDOTDIR:-$HOME}/.zprezto/modules/history-substring-search/external/zsh-history-substring-search.zsh
